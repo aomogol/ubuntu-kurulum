f3aca88c4fd8228ec7124b0ee3fdc67d0529ad13
Ahmet Onder Mogol <aomogol@gmail.com>
2021-07-11 10:51:42 +0300

    Update install-google-chrome-v1.sh

--- scriptler/install-google-chrome-v1.sh
+++ scriptler/install-google-chrome-v1.sh
@@ -11,15 +11,12 @@
 #
 ##################################################################################################################
 
-
 # donwloading and installing google chrome for netflix e.g.
-
 # rm /tmp/google-chrome-stable_current_amd64.deb
 
 # echo "downloading google chrome latest stable edition"
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
 # sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
-
 # rm /tmp/google-chrome-stable_current_amd64.deb
 
 
